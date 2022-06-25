f6d1cbb3528dd9f408951788f6d80debe70fb43d
madha <madha@192.168.0.106>
2022-06-25 13:00:09 +0530

    Localvariable

---
+++ java Demo/src/JavaVariable.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class JavaVariable 
+{
+  public void babyAge() 
+  {
+	int age=0;
+	age=age+7;
+	System.out.println("baby age is : " + age);
+	}
+   public static void main(String[] args) 
+	{
+	  JavaVariable test=new JavaVariable();
+	  test.babyAge();	   
+
+	}
+
+}
