a5037f0ef87dfcf56e4903f62b3a5c1b607e376d
madha <madha@LAPTOP-0AVNSC6S>
2022-06-26 18:12:13 +0530

    Datatype

---
+++ java Demo/src/Boolean.java
@@ -0,0 +1,10 @@
+public class Boolean 
+{
+	public static void main(String[]args) 
+	{
+		boolean var = false;//true or false(0or1)
+		System.out.println(var);
+		char var1 = '\u00A7';//$,*,.,
+		System.out.println(var1);
+	}
+}
---
+++ java Demo/src/Integer.java
@@ -0,0 +1,14 @@
+public class Integer 
+{
+	public static void main(String[]args) 
+	{
+		int max= 2147483647;
+		int min=-2147483648;
+		short shortMax=32767;
+		short shortmin=-32768;
+		long longmax=9223372036854775807L; 
+		long longmin=-9223372036854775808L;
+		byte bytemax=127;
+		byte bytemin=-128;		
+	}
+}
