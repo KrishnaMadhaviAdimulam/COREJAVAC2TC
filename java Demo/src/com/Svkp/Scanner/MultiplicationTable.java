911027f950288b811f5be1150fd62206e39188dd
madha <madha@192.168.0.101>
2022-07-18 13:19:55 +0530

    Multiplication Table

---
+++ java Demo/src/com/Svkp/Scanner/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package com.Svkp.Scanner;
+
+public class MultiplicationTable 
+{
+	public static void main(String[]args)
+	{
+		int num = 6;
+		for(int i=1; i<=10;i++)
+		{
+			System.out.println(num+"*"+i+"="+num*i);
+		}
+	}
+
+		
+}
